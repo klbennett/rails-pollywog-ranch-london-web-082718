8a44546185add8ac025fd50534179f041147c23a
Kay B <kaylouisebennett@gmail.com>
2018-09-27 20:59:51 +0000

    automatically backed up by learn

--- app/controllers/tadpoles_controller.rb
+++ app/controllers/tadpoles_controller.rb
@@ -4,7 +4,7 @@ class TadpolesController < ApplicationController
 
   def metamorphose
     @tadpole = Tadpole.find(params[:id])
-    @frog = Frog.create(name: @tadpole.name, color: @tadpole.color)
+    @frog = Frog.create(name: @tadpole.name, color: @tadpole.color, pond: @tadpole.pond)
     @frog.save
     @tadpole.destroy
     redirect_to frog_path(@frog)
